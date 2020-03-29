fe295e2b092ffee2610972113554e52bee4c5e9c
Seth Todd <seth.todd@outlook.com>
2020-03-29 05:09:58 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-  
+  include Dance
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
